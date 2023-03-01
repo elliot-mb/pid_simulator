e09c5a0103b7e36919faa1101d865957fd96d3a1
elliot-mb <elliotbuckingham14@gmail.com>
2023-03-01 11:38:09 +0000

    Added information in the presenter

--- include/presenter.hpp
+++ include/presenter.hpp
@@ -16,6 +16,16 @@
 This class transforms representations of components as collections of points, or single points and radii, to shapes and matrix transformations.
 
 These shapes and matrix transformations are then used by the view to render shapes
+
+.: Idea :.
+Use the visitor pattern, creating an interface DrawingVisitor, which Presenter implements, that has a visit method for every concrete subclass of Component;
+- Point mass
+- Beam
+- Disc
+- Motor
+- Spring
+- Slider
+These visit methods will create the respective transformation matrices for the view drawing pipeline; primitive shape + transformation = proper visual representation
 */
 
 class Presenter {
